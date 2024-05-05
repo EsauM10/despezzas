c7cddc3c61324038f3380e7167717830466e003a
EsauM10 <esau2096@gmail.com>
2024-05-04 22:50:44 -0300

    feat: add expense list

--- web/src/components/Expenses/index.tsx
+++ web/src/components/Expenses/index.tsx
@@ -1,4 +1,8 @@
+import styles from './styles.module.css'
 import { SectionCard } from '../SectionCard'
+
+
+const expenses: ExpenseDTO[] = []
 
 export default function Expenses() {
     return (
@@ -7,6 +11,25 @@ export default function Expenses() {
             text='todas as'
             strongText='despesas'
         >            
+            <ul className={styles.expenses}>
+                {expenses.map((item) => (
+                    <li key={item.id}>
+                        <div>
+                            <img src="" alt="" />
+                            <div>
+                                <h1>{item.title}</h1>
+                                <span>{item.status}</span>
+                                <p>{item.category}</p>
+                            </div>
+                        </div>
+                        <aside>
+                            <h1>único</h1>
+                            <h2>R$ {item.value}</h2>
+                            <p>{item.date}</p>
+                        </aside>
+                    </li>
+                ))}
+            </ul>
         </SectionCard>
     )
 }

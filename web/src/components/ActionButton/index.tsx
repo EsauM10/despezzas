f91c9f6e012e830a07c85ba57492de40017521ea
EsauM10 <esau2096@gmail.com>
2024-10-12 21:12:45 -0300

    refactor: update component wrapper to avoid nested errors

--- web/src/components/ActionButton/index.tsx
+++ web/src/components/ActionButton/index.tsx
@@ -10,10 +10,10 @@ interface ActionButtonProps {
 
 export default function ActionButton({ title, subtitle, icon, color, onClick }: ActionButtonProps) {
     return (
-        <button className={styles.container} onClick={onClick}>
+        <div className={styles.container} onClick={onClick}>
             <i className={icon} style={{color: color}}></i>
             <h2>{ title }</h2>
             <h3>{ subtitle }</h3>
-        </button>
+        </div>
     )
 }
